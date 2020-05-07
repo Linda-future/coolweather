1ff25c92fab86ec4a3e8e645418067734609679a
Lilin <1843207546@qq.com>
2020-05-07 22:03:45 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/linda/CoolWeather/db/City.java
@@ -0,0 +1,42 @@
+package com.linda.CoolWeather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class City extends DataSupport {
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int piovinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getPiovinceId() {
+        return piovinceId;
+    }
+
+    public void setPiovinceId(int piovinceId) {
+        this.piovinceId = piovinceId;
+    }
+}
